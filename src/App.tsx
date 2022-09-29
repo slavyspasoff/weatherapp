ead3dabc24f090bfcf1b8d2ba7730c28afca6e32
Slavy Spasoff <slavyspasoff@gmail.com>
2022-09-29 16:49:21 +0200

    add data.json for seeding data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from 'react';
 function App() {
+  const [data, setData] = useState<object>({});
+  useEffect(() => {
+    (async () => {
+      const req = await import('./data.json');
+      setData(req.current);
+    })();
+  }, []);
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      <h1></h1>
     </div>
   );
 }
