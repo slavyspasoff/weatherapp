2d467db1380aef701fc9e2dd623a59fdd6cfa2e2
Slavy Spasoff <slavyspasoff@gmail.com>
2023-01-14 16:19:23 +0100

    update AlertCard

--- src/components/Today/AlertCard.tsx
+++ src/components/Today/AlertCard.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from '@mui/material';
+import { ReportGmailerrorred } from '@mui/icons-material';
 
 import CardBack from '../../styles/Card/CardBack.styles';
 import { Alert } from '../../types/global.types';
@@ -6,7 +7,7 @@ import { Alert } from '../../types/global.types';
 //TODO: Move to IntlHelper
 const formatDate = (v: number) => {
    return new Intl.DateTimeFormat([navigator.language], {
-      dateStyle: 'full',
+      dateStyle: 'long',
    }).format(new Date(v * 1000));
 };
 
@@ -16,16 +17,21 @@ function AlertCard({ description, start, end, sender_name, event, tags }: Props)
    return (
       <CardBack sx={(theme) => ({ padding: theme.spacing(2) })}>
          <Box>
-            <Typography sx={(theme) => ({ fontSize: '1.5rem' })} component={'h3'}>
-               Alert:
+            <Typography
+               sx={(theme) => ({ fontSize: '1.5rem', lineHeight: '3rem' })}
+               component={'h3'}
+            >
+               <ReportGmailerrorred /> Alert
             </Typography>
          </Box>
          <Box>
-            <Typography>Start date: {formatDate(end)}</Typography>
-            <Typography>End date: {formatDate(end)}</Typography>
-            {sender_name && <Typography>Sender: {sender_name}</Typography>}
-            <Typography>Event: {event}</Typography>
-            {description && <Typography>Description: {description}</Typography>}
+            <Typography>
+               {formatDate(start)} - {formatDate(end)}
+            </Typography>
+            {sender_name && <Typography>{sender_name}</Typography>}
+            <Typography>{event[0].toUpperCase().concat(event.slice(1))}</Typography>
+            <br />
+            {description && <Typography>{description}</Typography>}
          </Box>
       </CardBack>
    );
